bf8c19ca555d8730db33c69ff3c767acecfdcba4
jackhasakeyboard <jackhasakeyboard@gmail.com>
2018-04-12 22:24:43 -0600

    Fibonacci Sequence

---
+++ numbers/fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	printf("Number: ");
+	int no;
+	scanf("%d", &no);
+
+	int t1 = 0,
+			t2 = 1;
+
+	while (t1 + t2 < no) {
+		t1 += t2;
+
+		printf("%d\n", t1);
+
+		t2 = t1 - t2;
+	}
+
+	return 0;
+}
